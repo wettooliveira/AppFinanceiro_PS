4806ba43144a0fab75028677b215dcd1682c0831
user <user@DESKTOP-97L1SFR>
2021-10-21 19:51:13 -0300

    Adicionar arquivos de projeto.

---
+++ AppFinanceiroPS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppFinanceiroPS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
